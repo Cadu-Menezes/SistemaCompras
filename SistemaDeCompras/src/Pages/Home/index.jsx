7e156f143188b605e57cbfdd7da65857d9dd04f5
Cadu-Menezes <cadumenezes051@gmail.com>
2024-08-13 20:06:41 -0300

    implementando uma api

--- SistemaDeCompras/src/Pages/Home/index.jsx
+++ SistemaDeCompras/src/Pages/Home/index.jsx
@@ -1,5 +1,5 @@
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import React from "react";
 
 const StyledHome = styled.div`
   background-color: #f0f0f0;
@@ -32,19 +32,49 @@ const Text = styled.p`
   font-size: 1.2rem;
   color: #666;
   text-align: center;
+  margin-bottom: 1rem;
 
   @media (min-width: 768px) {
     font-size: 1.5rem;
   }
 `;
 
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  color: #fff;
+  background-color: #007bff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
 const Home = () => {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.error("Erro ao buscar conselho:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice(); // Fetches an initial advice on component mount
+  }, []);
+
   return (
     <StyledHome>
       <Title>Projeto do caduzinho!</Title>
-      <Text>
-        Aqui você vai encontrar a metologia XGH no mais alto nível.
-      </Text>
+      <Text>{advice}</Text>
+      <Button onClick={fetchAdvice}>Gerar Novo Conselho</Button>
     </StyledHome>
   );
 };
